0817a6b8fc5d93df4a7f2025314e4f234829c0f1
Drew Wilson <drewwilson@DWILSON-MACBOOKPRO-PATTERN.local>
2022-05-19 14:53:49 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :articles
+
+  # Defines the root path route ("/")
+  root "welcome#index"
+end
